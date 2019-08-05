88b4ab058a71d88ff74f97f691ae3c995ada43b7
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-05 15:08:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,28 +14,34 @@ class Artist
     @@all 
   end 
   
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name 
+    artist
+  end 
+  
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end 
+  
   def add_song(song)
     @song << song 
-    song.artist = self 
-    song  
-  end 
+     
+     end 
+     
+     def print_songs
+       songs.each { |song| puts song.name } 
+     end 
   
  def save
    @@all << self
-   self 
   end 
   
-  def self.all 
-    @@all 
-  end 
-  
-  def self.find_or_creat_by_name(name)
-    self.all.detect {|artist| artist.name == name} ||
-    Artist.new(name).save
-  end 
-  
-  def print_songs
-    self.songs.each {|song| puts song.name}
-  end
+  def songs 
+    @songs 
 end 
 end 
