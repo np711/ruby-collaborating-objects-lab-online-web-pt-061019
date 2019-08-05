f10ab042ebc4070c8de4a54d92efc5441f0ac47a
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-05 15:03:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
-require 'pry'
+
 
 class Artist 
   attr_accessor :name
   @@all = [] 
+  
   def initialize(name)
     @name = name 
     @songs = []
+    save 
+  end 
+  
+  def self.all
+    @@all 
   end 
   
   def add_song(song)
